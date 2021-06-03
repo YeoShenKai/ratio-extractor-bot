aa064c9299eda07cc85e8c8fbd6acf0c573e30fc
Shen Kai <yeoshenkai@gmail.com>
2021-06-03 13:14:39 +0800

    Main

--- RatioAnalyserBot.py
+++ RatioAnalyserBot.py
@@ -1,13 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import seaborn as sns
+import pandas as pd
 
-x = np.array([1, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9])
-y = np.array([13, 14, 17, 12, 23, 24, 25, 25, 24, 28, 32, 33])
+#csv file --> array. *Indexing: [columns][row]
+data = pd.read_csv('CompanyScreeningReport (3).csv', header=1).values
+data = pd.DataFrame(data)
 
-plt.plot(x,y, 'o')
-m, b = np.polyfit(x, y, 1)
-print(m, 'x +', b)
-plt.plot(x, m*x + b)
-plt.show()
-#z = sns.regplot(x,y, ci=None)
+#Helper function to check if a value in a cell is a float - returns False if cell is empty or NM
+def isnumber(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False
+
+#Function that converts all the values of a DataFrame column into a float
+def to_float(column):
+    return list(filter(lambda x: isnumber(x), column))
+
+#Functions to return various metrics of a particular column index 
+def total(columnindex):
+    return np.sum(list(map(lambda x: float(x), filter(lambda x: isnumber(x), data[columnindex][1:]))))
+
+def average(columnindex):
+    return np.mean(list(map(lambda x: float(x), filter(lambda x: isnumber(x), data[columnindex][1:]))))
+
+#Function taking in 2 column indexes, and then determining the correl coeff (r value) for the two sets of data
+def correl(colindex1, colindex2):
+    pass
